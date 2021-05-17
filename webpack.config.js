9f30ea453a04feec0bc54e13769556ffe63c1628
Kumar Rangarajan <kumarrangarajan@Kumars-MacBook-Pro.local>
2021-05-17 18:58:29 -0400

    updated indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,47 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(js)$/, 
-                use: 'babel-loader' 
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js)$/, 
+        use: 'babel-loader' 
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            {
-                test: /\.css$/i,
-                exclude: /node_modules/,
-                use: [
-                  'style-loader',
-                  {
-                    loader: 'css-loader',
-                    options: {
-                      modules: true,
-                    },
-                  },
-                ],
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    mode: 'development',
-    devServer: {
-        historyApiFallback: true,
-        port: 3000
-    },
-    plugins: [
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
+          },
+        ],
+      }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    port: 3000
+  },
+  plugins: [
+    
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
